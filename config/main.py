58baf32f3881cda91bd4b5a5993f794ef5da2a69
Lukas Stockner <lstockner@genesiscloud.com>
2022-06-13 18:06:27 +0200

    Add support for fast_rate option on PortChannels

--- config/main.py
+++ config/main.py
@@ -1900,8 +1900,9 @@ def portchannel(ctx, namespace):
 @click.argument('portchannel_name', metavar='<portchannel_name>', required=True)
 @click.option('--min-links', default=1, type=click.IntRange(1,1024))
 @click.option('--fallback', default='false')
+@click.option('--fast-rate', default='false')
 @click.pass_context
-def add_portchannel(ctx, portchannel_name, min_links, fallback):
+def add_portchannel(ctx, portchannel_name, min_links, fallback, fast_rate):
     """Add port channel"""
     if is_portchannel_name_valid(portchannel_name) != True:
         ctx.fail("{} is invalid!, name should have prefix '{}' and suffix '{}'"
@@ -1919,6 +1920,8 @@ def add_portchannel(ctx, portchannel_name, min_links, fallback):
         fvs['min_links'] = str(min_links)
     if fallback != 'false':
         fvs['fallback'] = 'true'
+    if fast_rate != 'false':
+        fvs['fast_rate'] = 'true'
     db.set_entry('PORTCHANNEL', portchannel_name, fvs)
 
 @portchannel.command('del')
